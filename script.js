2895f66d16dc2edd9ac5e46cdab734ac2a843d49
Barsoum <barsoumadly@gmail.com>
2023-08-17 18:23:05 +0300

    calc age

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+// Selecting input field
+const dateInput = document.getElementById('date-input');
+
+// Selecting button
+const btnCalc = document.querySelector('.btn-calc');
+
+// Selecting output fields
+const yearsEL = document.getElementById('years');
+const monthsEL = document.getElementById('months');
+const daysEL = document.getElementById('days');
+
+// Calculating difference between dates
+const calcDifference = function (birthDate) {
+  const now = new Date();
+  const years = now.getFullYear() - birthDate.getFullYear();
+  const months = now.getMonth() - birthDate.getMonth();
+  const days = now.getDate() - birthDate.getDate();
+  return [years, months, days];
+};
+
+// Showing age
+const showAge = function (years, months, days) {
+  yearsEL.textContent = years;
+  monthsEL.textContent = months;
+  daysEL.textContent = days;
+};
+
+btnCalc.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const birthDate = dateInput.value;
+  if (birthDate) {
+    const age = calcDifference(new Date(birthDate));
+    showAge(...age);
+  }
+  dateInput.value = '';
+});
